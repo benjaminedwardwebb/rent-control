890865d815cbc93c44e21cd3df85520af14a1667
Benjamin Edward Webb <benjaminedwardwebb@gmail.com>
2018-05-16 22:46:17 -0500

    added zipcode page, controller, and search into navbar (still needs some work like working validations, etc)

---
+++ app/controllers/SearchController.scala
@@ -0,0 +1,59 @@
+package controllers
+
+import javax.inject.Inject
+
+import models.Widget
+import play.api.data._
+import play.api.i18n._
+import play.api.mvc._
+
+/**
+ * The classic WidgetController using MessagesAbstractController.
+ *
+ * Instead of MessagesAbstractController, you can use the I18nSupport trait,
+ * which provides implicits that create a Messages instance from a request
+ * using implicit conversion.
+ *
+ * See https://www.playframework.com/documentation/2.6.x/ScalaForms#passing-messagesprovider-to-form-helpers
+ * for details.
+ */
+class SearchController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
+  import ZipcodeSearch._
+
+  // The URL to the widget.  You can call this directly from the template, but it
+  // can be more convenient to leave the template completely stateless i.e. all
+  // of the "WidgetController" references are inside the .scala file.
+  private val postUrl = routes.SearchController.search()
+
+  def index = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.index(form, postUrl))
+  }
+
+  def listSearch = Action { implicit request: MessagesRequest[AnyContent] =>
+    // Pass an unpopulated form to the template
+    Ok(views.html.zipcode(form, postUrl))
+  }
+
+  // This will be the action that handles our form post
+  def search = Action { implicit request: MessagesRequest[AnyContent] =>
+    val errorFunction = { formWithErrors: Form[Data] =>
+      // This is the bad case, where the form had validation errors.
+      // Let's show the user the form again, with the errors highlighted.
+      // Note how we pass the form with errors to the template.
+      BadRequest(views.html.zipcode(formWithErrors, postUrl))
+    }
+
+    val successFunction = { data: Data =>
+	  Ok(views.html.zipcode(form, postUrl))
+      // This is the good case, where the form was successfully parsed as a Data object.
+	  /*
+      val widget = Widget(name = data.name, price = data.price)
+      widgets.append(widget)
+      Redirect(routes.WidgetController.listWidgets()).flashing("info" -> "Widget added!")
+	  */
+    }
+
+    val formValidationResult = form.bindFromRequest
+    formValidationResult.fold(errorFunction, successFunction)
+  }
+}
---
+++ app/controllers/ZipcodeSearch.scala
@@ -0,0 +1,38 @@
+package controllers
+
+object ZipcodeSearch {
+	import play.api.data.Forms._
+	import play.api.data.Form
+
+	/**
+	* A form processing DTO that maps to the form below.
+	*
+	* Using a class specifically for form binding reduces the chances
+	* of a parameter tampering attack and makes code clearer.
+	*/
+	case class Data(zipcode: String)
+
+	def validate(zipcode: String): Option[Data] = {
+		val pattern = "\\d{5}".r
+		zipcode match {
+			case pattern(_*) => Some(Data(zipcode))
+			case _ => None
+		}
+	}
+
+	/**
+	* The form definition for the "create a widget" form.
+	* It specifies the form fields and their types,
+	* as well as how to convert from a Data to form data and vice versa.
+	*/
+	val form = Form(
+		mapping(
+			"zipcode" -> text
+		)(Data.apply)(Data.unapply) verifying(
+			"Invalid", 
+			fields => fields match {
+				case data => validate(data.zipcode).isDefined
+			}
+		)
+	)
+}
